8427925db31b60241aa09f0d14718520e35d48cf
Cole Erickson <pawnstar4@gmail.com>
2016-10-10 14:09:42 -0600

    Started to add login stuff

--- js/scripts.js
+++ js/scripts.js
@@ -19,9 +19,27 @@ $(document).ready(function(){
        window.unmaximize();
      }
   })
+  $('#getData').click(function(ev){
+    ev.preventDefault();
+    remote.getCurrentWindow().webContents.openDevTools();
+    $.ajax('https://students.cs.byu.edu/~cs236ta/helpqueue/getStatus.php?id=egm8',{
+      success:function(data){
+        console.log(data);
+      }
+    })
+    return false;
+  })
   $('#openDevTools').click(function(ev){
     ev.preventDefault();
     remote.getCurrentWindow().webContents.openDevTools();
+    $.ajax('https://cas.byu.edu/cas/login?service=https%3A%2F%2Fstudents.cs.byu.edu%2F%7Ecs236ta%2Fhelpqueue%2F%3Flogin%3D',{
+      success:function(data){
+        console.log(data);
+      },
+      error:function(result){
+        console.log(result);
+      }
+    })
     return false;
   })
 })
