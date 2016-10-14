650354014da33048767b1ef40b32a946568fd189
Cole Erickson <pawnstar4@gmail.com>
2016-10-13 19:22:38 -0600

    Small changes at work.

--- js/ui.js
+++ js/ui.js
@@ -9,19 +9,13 @@ var uiLoading = function(){
 }
 
 var uiData = function(data){
+  var newData = data;
+  newData.stats = 'too long to show';
   $('#mainContent').html('<h1>CS 236 Help Queue Demo</h1>' +
     studentsBeingHelped({list: data.list.filter(function(student){
       return student.beingHelpedBy;
     }), moment: moment}) +
     studentsWaiting({list: data.list.filter(function(student){
       return !student.beingHelpedBy;
-    }), moment: moment}) + '<div class="alert alert-success"><pre>' + JSON.stringify(data, null, 2) + '</pre></div>');
-}
-
-var makeTable = function(data){
-  var stuff = data.list.reduce(function(prev, current){
-    return prev + '<tr><td>' + current.name + '</td><td>' + current.netId + '</td><td>' + current.question + '</td><td>' + current.enqueueTime + '</td></tr>';
-  }, '<div class=""><table class="table"><tr><th>Name</th><th>NetID</th><th>Question</th><th>Time waiting</th></tr>') + '</table></div>';
-  console.log(stuff);
-  return stuff;
+    }), moment: moment}) + '<div class="alert alert-success"><pre>' + JSON.stringify(newData, null, 2) + '</pre></div>');
 }
